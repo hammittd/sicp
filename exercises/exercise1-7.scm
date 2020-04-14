c180871bf78bb658a868d4f5b93d5ee37aeaaba8
Derek Hammitt <derekrhammitt@gmail.com>
2020-04-14 03:36:55 +0000

    exercise 1-7

--- exercises/exercise1-7.scm
+++ exercises/exercise1-7.scm
@@ -3,41 +3,40 @@
 ;; The good enough test used in computing square roots will not be very effective
 ;; for finding the square roots
 ;;
+;; Also, in real computers, arithmetic operations are almost always performed with
+;; limited precision.
 ;;
+;; This makes our test inadequate for very large numbers. Explain these statements,
+;; with examples showing how the test fails for small and large numbers.
 ;;
-(define (new-if predicate then-clause else-clause)
-  (cond (predicate then-clause)
-        (else else-clause) ))
-;; Eva demonstrates the program for Alyssa:
-(new-if (= 2 3) 0 5)
-(new-if (= 1 1) 0 5)
-
-;; Delighted, Alyssa uses new-if to rewire the square-root program:
-
-(define (square x)
-  (* x x) )
-
-(define (good-enough? guess x)
-  (< (abs (- (square guess) x)) 0.001) )
-
-(define (average x y)
-  (/ (+ x y) 2) )
-
-(define (improve guess x)
-  (average guess (/ x guess) ))
+(define (sqrt x)
+  (sqrt-iter 1.0 x))
 
 (define (sqrt-iter guess x)
-  (new-if (good-enough? guess x)
+  (if (good-enough? guess x)
       guess
       (sqrt-iter (improve guess x)
-                 x )))
+                 x)))
 
-(define (sqrt x)
-  (sqrt-iter 1.0 x) )
+(define (improve guess x)
+  (average guess (/ x guess)))
 
-;; The program never terminates.
-;; Why? Because our interpreter uses applicative order evaluation.
-;; This means that the consequent clause to new-if's else predicate
-;; will be evaluated every time it is called. In this case, the else's
-;; consequent is another call of sqrt-iter, which includes another call
-;; of new-if, and so on until we have a loop that can't stop calling itself.
+(define (average x y)
+  (/ (+ x y) 2))
+
+;; initial definition of good-enough?
+;; (define (good-enough? guess x)
+;;   (< (abs (- (square guess) x)) 0.001))
+
+(define (square x)
+  (* x x))
+
+;; test:
+;; (sqrt .000000009)
+;; > 0.03125009590619131
+
+;; new definition of good-enough?
+;; Measuring whether there is a perceptible difference between an
+;; initial guess and the improved value
+(define (good-enough? guess x)
+  (= (improve guess x) guess))
